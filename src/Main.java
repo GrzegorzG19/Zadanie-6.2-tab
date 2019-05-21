53fd0d24cb795e38e8df55ee73e27114bc5d8883
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-21 13:32:50 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] tab = {"Jan", "Krzyś", "Przemek"};
+        System.out.println(tab[0]);
+        System.out.println(tab[1]);
+        System.out.println(tab[2]);
+    }
+}
